123f6da42963fae9dd0134f03e28e0ec1a6c6d25
tiwratnesh <72146960+tiwratnesh@users.noreply.github.com>
2020-10-03 09:58:27 +0530

    Update install_services.sh

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+whoami  >> /tmp/vulnado.log
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose > /tmp/vulnado.log
 sudo chmod +x /usr/bin/docker-compose >> /tmp/vulnado.log
