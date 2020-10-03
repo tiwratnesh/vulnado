01ff8ce94f5663e88ea1e17172c7380d491691cc
tiwratnesh <72146960+tiwratnesh@users.noreply.github.com>
2020-10-03 09:58:01 +0530

    Update run_dast.sh

--- scripts/run_dast.sh
+++ scripts/run_dast.sh
@@ -1,2 +1,3 @@
 cd /tmp
-sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1):1337/login.html -g gen.conf -r zap_testreport.html >/dev/null 2>&1 &
+whoami  >> /tmp/vulnado.log
+sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1):1337/login.html -g gen.conf -r zap_testreport.html  >> /tmp/vulnado.log >/dev/null 2>&1 &
